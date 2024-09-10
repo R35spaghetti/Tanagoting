3b770a78fd0147bde8e57dd2b8c8bf6795467975
RB <rkrb@tyfonmail.se>
2024-09-10 15:53:04 +0200

    generate random letters

--- Tanagoting/Program.cs
+++ Tanagoting/Program.cs
@@ -2,6 +2,16 @@
 Console.WriteLine("GoodJNDJOIGDbye, World!");
 Console.WriteLine("Sadface.jpg");
 Console.WriteLine("eep");
+string letters = "gevhtriyvg";
+
+Console.WriteLine(GenerateRandomLetters(5));
+string GenerateRandomLetters(int length)
+{
+    return new string(
+        Enumerable.Repeat(letters, length)
+            .Select(letterString => letterString[new Random().Next(letterString.Length)])
+            .ToArray());
+}
 
 var x = 5;
 Console.WriteLine(@$"Hello there i got {x} houses");
