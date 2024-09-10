2563312fd2ee0a74469ca48a33182db4fe7494ed
RB <rkrb@tyfonmail.se>
2024-09-10 15:40:49 +0200

    TEST

--- Tanagoting/Program.cs
+++ Tanagoting/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Goodbye, World!");
 Console.WriteLine("Sadface.jpg");
+Console.WriteLine("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH");
